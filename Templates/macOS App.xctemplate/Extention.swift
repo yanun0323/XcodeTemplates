0c737624999d78fd2b2df843560571638b574561
yanun0323 <a1256212562@gmail.com>
2022-11-30 05:58:32 +0800

    update macos extension

--- Templates/macOS App.xctemplate/Extention.swift
+++ Templates/macOS App.xctemplate/Extention.swift
@@ -1,1 +1,25 @@
 import SwiftUI
+import AppKit
+
+extension NSTableView {
+    open override func viewDidMoveToWindow() {
+        super.viewDidMoveToWindow()
+        
+        backgroundColor = NSColor.clear
+        enclosingScrollView!.drawsBackground = false
+        enclosingScrollView!.hasVerticalScroller = false
+        enclosingScrollView!.hasHorizontalScroller = false
+    }
+}
+
+extension NSTextView {
+    open override var frame: CGRect {
+        didSet {
+            backgroundColor = .clear
+            drawsBackground = false
+            enclosingScrollView?.drawsBackground = false
+            enclosingScrollView?.hasVerticalScroller = false
+            enclosingScrollView?.hasHorizontalScroller = false
+        }
+    }
+}
