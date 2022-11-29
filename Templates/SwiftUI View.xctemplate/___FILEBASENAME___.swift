faa8be4077142cc8936c0600b499c91466c2c89d
yanun0323 <a1256212562@gmail.com>
2022-11-30 07:57:34 +0800

    add swiftUI file templates

---
+++ Templates/SwiftUI View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    var body: some View {
+       /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER___()
+    }
+}
